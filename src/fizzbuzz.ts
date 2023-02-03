92785cf306a1470748147238752e118e31e3204e
Joshua Kawamata <email>
2023-02-03 11:08:16 +0900

    fix: 出力をfizz, buzz, fizzbuzzのみに変更

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,11 +1,11 @@
 function fizzBuzz(num: number): void {
 	for (let i = 1; i <= num; i++) {
 		if (i % 5 == 0 && i % 3 == 0) {
-			console.log(`${i} is FizzBuzz`);
+			console.log(`FizzBuzz`);
 		} else if (i % 3 == 0) {
-			console.log(`${i} is Fizz`);
+			console.log(`Fizz`);
 		} else if (i % 5 == 0) {
-			console.log(`${i} is Buzz`);
+			console.log(`Buzz`);
 		} else {
 			console.log(i);
 		}
