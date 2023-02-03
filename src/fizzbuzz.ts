6982a0b3d1e7f8eda8a7668827ecd217cce3420a
Joshua Kawamata <email>
2023-02-03 17:20:02 +0900

    feature: changed equality check loose→strict equality

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,10 +1,10 @@
 function fizzBuzz(num: number): void {
 	for (let i = 1; i <= num; i++) {
-		if (i % 5 == 0 && i % 3 == 0) {
+		if (i % 5 === 0 && i % 3 === 0) {
 			console.log(`FizzBuzz`);
-		} else if (i % 3 == 0) {
+		} else if (i % 3 === 0) {
 			console.log(`Fizz`);
-		} else if (i % 5 == 0) {
+		} else if (i % 5 === 0) {
 			console.log(`Buzz`);
 		} else {
 			console.log(i);
