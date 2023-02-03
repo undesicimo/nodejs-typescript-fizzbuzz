4a96e7e9e9e0b5914eeb7e5706fe28e1e86d3fbb
Joshua Kawamata <email>
2023-02-03 12:50:16 +0900

    test: added test for false arguments

--- isZero.test.ts
+++ isZero.test.ts
@@ -1,1 +1,11 @@
 import { isZero } from './src/isZero';
+
+test('0を渡したらtrueになること', () => {
+	const result = isZero(0);
+	expect(result).toBe(true);
+});
+
+test('1を渡したら、falseになること', () => {
+	const result = isZero(1);
+	expect(result).toBe(false);
+});
