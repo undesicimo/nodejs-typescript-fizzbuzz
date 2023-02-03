5a9352f73eb2b1b198a34c33d18a0f254ff3f3df
Joshua Kawamata <email>
2023-02-03 18:03:19 +0900

    refactor: naming change

--- src/generateMessage.test.ts
+++ src/generateMessage.test.ts
@@ -2,23 +2,23 @@ import { generateMessage } from './generateMessage';
 
 describe('Check for generateMessage return value', () => {
 	it('should return FizzBuzz for divison of 3 and 5', () => {
-		const inputNumber = 15;
-		const result = generateMessage(inputNumber);
+		const input = 15;
+		const result = generateMessage(input);
 		expect(result).toBe('FizzBuzz');
 	});
 	it('should return Fizz for division of 3', () => {
-		const inputNumber = 9;
-		const result = generateMessage(inputNumber);
+		const input = 9;
+		const result = generateMessage(input);
 		expect(result).toBe('Fizz');
 	});
 	it('should return Buzz for division of 5', () => {
-		const inputNumber = 10;
-		const result = generateMessage(inputNumber);
+		const input = 10;
+		const result = generateMessage(input);
 		expect(result).toBe('Buzz');
 	});
 	it('should return the number for non dividable to 3 and 5', () => {
-		const inputNumber = 8;
-		const result = generateMessage(inputNumber);
+		const input = 8;
+		const result = generateMessage(input);
 		expect(result).toBe('8');
 	});
 });
