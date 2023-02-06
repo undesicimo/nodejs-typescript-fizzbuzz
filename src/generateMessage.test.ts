511604836e6c00599abdf5651d7b8213ef606d2c
Joshua Kawamata <email>
2023-02-06 17:05:24 +0900

    test: added test for minimum (1) and maximum(30) values

--- src/generateMessage.test.ts
+++ src/generateMessage.test.ts
@@ -21,4 +21,14 @@ describe('Check for generateMessage return value', () => {
 		const result = generateMessage(input);
 		expect(result).toBe('8');
 	});
+	it('should return the same number for minimum(1) boundary value', () => {
+		const input = 1;
+		const result = generateMessage(input);
+		expect(result).toBe('1');
+	});
+	it('should return FizzBuzz for maximum(30) boundary value', () => {
+		const input = 30;
+		const result = generateMessage(input);
+		expect(result).toBe('FizzBuzz');
+	});
 });
