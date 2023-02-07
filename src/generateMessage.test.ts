91e8a62663b740ac78308c857d2207637787d2c9
Joshua Kawamata <email>
2023-02-07 12:10:24 +0900

    refactor: added test title and table format tests

--- src/generateMessage.test.ts
+++ src/generateMessage.test.ts
@@ -1,65 +1,52 @@
 import { generateMessage } from './generateMessage';
 
-describe('Check for generateMessage return value', () => {
-	it('should return FizzBuzz for divison of 3 and 5', () => {
-		const input = 15;
-		const result = generateMessage(input);
-		expect(result).toBe('FizzBuzz');
-	});
-	it('should return Fizz for division of 3', () => {
-		const input = 9;
-		const result = generateMessage(input);
-		expect(result).toBe('Fizz');
-	});
-	it('should return Buzz for division of 5', () => {
-		const input = 10;
-		const result = generateMessage(input);
-		expect(result).toBe('Buzz');
-	});
-	it('should return the number for non dividable to 3 and 5', () => {
-		const input = 8;
-		const result = generateMessage(input);
-		expect(result).toBe('8');
-	});
-	it('should return the same number for minimum(1) boundary value', () => {
-		const input = 1;
-		const result = generateMessage(input);
-		expect(result).toBe('1');
-	});
-	it('should return FizzBuzz for maximum(30) boundary value', () => {
-		const input = 30;
-		const result = generateMessage(input);
-		expect(result).toBe('FizzBuzz');
-	});
-});
-
 const testCases = [
 	{
+		title: 'should return the same number for minimum(1) boundary value',
 		inputNumber: 1,
 		expectedResult: '1',
 	},
 	{
-		inputNumber: 8,
-		expectedResult: '8',
-	},
-	{
+		title: 'should return FizzBuzz for maximum(30) boundary value',
 		inputNumber: 30,
 		expectedResult: 'FizzBuzz',
 	},
 	{
+		title: 'should return the number for non dividable to 3 and 5',
+		inputNumber: 8,
+		expectedResult: '8',
+	},
+	{
+		title: 'should return FizzBuzz for divison of 3 and 5',
+		inputNumber: 15,
+		expectedResult: 'FizzBuzz',
+	},
+	{
+		title: 'should return Fizz for division of 3',
 		inputNumber: 9,
 		expectedResult: 'Fizz',
 	},
 	{
+		title: 'should return Buzz for division of 5',
 		inputNumber: 5,
 		expectedResult: 'Buzz',
 	},
 ];
 
-test.each(testCases)(
-	' Parameterized fizzbuzz test',
-	({ inputNumber, expectedResult }) => {
-		const fizzBuzzOutput = generateMessage(inputNumber);
-		expect(fizzBuzzOutput).toBe(expectedResult);
-	}
-);
+test.each(testCases)(' it $title', ({ inputNumber, expectedResult }) => {
+	const fizzBuzzOutput = generateMessage(inputNumber);
+	expect(fizzBuzzOutput).toBe(expectedResult);
+});
+
+test.each`
+	inputNumber | expectedResult | title
+	${1}        | ${'1'}         | ${'(table)should return the same number for minimum(1) boundary value'}
+	${8}        | ${'8'}         | ${'(table)should return the number for non dividable to 3 and 5'}
+	${30}       | ${'FizzBuzz'}  | ${'(table)should return FizzBuzz for maximum(30) boundary value'}
+	${15}       | ${'FizzBuzz'}  | ${'(table)should return FizzBuzz for divison of 3 and 5'}
+	${9}        | ${'Fizz'}      | ${'(table)should return Fizz for division of 3'}
+	${5}        | ${'Buzz'}      | ${'(table)should return Buzz for division of 5'}
+`(' it $title', ({ inputNumber, expectedResult }) => {
+	const fizzBuzzOutput = generateMessage(inputNumber);
+	expect(fizzBuzzOutput).toBe(expectedResult);
+});
